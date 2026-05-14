119628d3152b92a6721b4d4daae11f5459ade200
RahulBurugoju <rahul9399555@gmail.com>
2026-05-14 22:49:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,35 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll(".prices");
+
+  let total = 0;
+
+  prices.forEach((price) => {
+    total += Number(price.textContent);
+  });
+
+  const oldRow = document.getElementById("total-row");
+
+  if (oldRow) {
+    oldRow.remove();
+  }
+
+  
+  const tr = document.createElement("tr");
+  tr.id = "total-row";
+
+  const td = document.createElement("td");
+  td.colSpan = 2;
+
+  td.textContent = `Total Price: Rs ${total}`;
+
+  tr.appendChild(td);
+
+  document
+    .getElementById("grocery-table")
+    .appendChild(tr);
+	
   
 };
 
